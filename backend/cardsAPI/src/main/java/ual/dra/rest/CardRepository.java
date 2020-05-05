d4d1e96d365b76b5c145ffb1d0d85754efdf81ce
Manel Mena <manel.mena@ual.es>
2020-05-05 17:18:26 +0200

    changed cards API cors

--- backend/cardsAPI/src/main/java/ual/dra/rest/CardRepository.java
+++ backend/cardsAPI/src/main/java/ual/dra/rest/CardRepository.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin
 @RepositoryRestResource()
 public interface CardRepository extends CrudRepository<Card, String> {
 
--- backend/cardsAPI/src/main/java/ual/dra/rest/RegionRepository.java
+++ backend/cardsAPI/src/main/java/ual/dra/rest/RegionRepository.java
@@ -2,7 +2,9 @@ package ual.dra.rest;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin
 @RepositoryRestResource()
 public interface RegionRepository extends CrudRepository<Region, String> {
 	
