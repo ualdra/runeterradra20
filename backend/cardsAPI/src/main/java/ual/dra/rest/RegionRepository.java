e5f1619e0dffc584e0a2b7b8b9b915de89bc6195
Manel Mena <manel.mena@ual.es>
2020-05-05 17:28:58 +0200

    added cors

--- backend/cardsAPI/src/main/java/ual/dra/rest/RegionRepository.java
+++ backend/cardsAPI/src/main/java/ual/dra/rest/RegionRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-@CrossOrigin
+
 @RepositoryRestResource()
 public interface RegionRepository extends CrudRepository<Region, String> {
 	
