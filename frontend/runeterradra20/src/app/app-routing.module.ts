fc1f8b5893e2257c4654042a875ae4bbb14510fd
Adalid Villanueva <avillanueva047@gmail.com>
2020-05-06 16:22:44 +0200

    componente registro añadido

--- frontend/runeterradra20/src/app/app-routing.module.ts
+++ frontend/runeterradra20/src/app/app-routing.module.ts
@@ -4,10 +4,10 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'login', loadChildren: () => import('./login-module/login-module.module').then( m => m.LoginModuleModule)
+    path: '', loadChildren: () => import('./login-module/login-module.module').then( m => m.LoginModuleModule)
   },
   {
-    path:'', redirectTo: 'login', pathMatch: 'full'
+    path:'', redirectTo: '', pathMatch: 'full'
   }
 ];
 
--- frontend/runeterradra20/src/app/login-module/login-module.module.ts
+++ frontend/runeterradra20/src/app/login-module/login-module.module.ts
@@ -4,14 +4,18 @@ import { LoginModuleComponent } from './login-module.component';
 import { LoginComponent } from '../login/login.component';
 import { LoginRoutingModule } from './login-module-routing.module'
 import { LoginService } from '../login.service';
+import { HttpClientModule } from '@angular/common/http';
+import { RegisterComponent } from '../register/register.component';
 
 @NgModule({
   imports: [
     CommonModule,
-    LoginRoutingModule
+    LoginRoutingModule,
+    HttpClientModule,
   ],
   declarations: [
     LoginModuleComponent,
+    RegisterComponent,
     LoginComponent
   ],
   providers: [
