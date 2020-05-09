bc723df6ecf943a1f43975736025ce53ed882f75
Alejandro Francisco García Uclés <agu528@inlumine.ual.es>
2020-05-09 20:43:57 +0200

    Cambios en el css, html y obtencion de imagenes.-Alejandro

--- frontend/runeterradra20/src/app/game-replay/game.service.ts
+++ frontend/runeterradra20/src/app/game-replay/game.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { analyzeAndValidateNgModules } from '@angular/compiler';
+
 
 @Injectable({
   providedIn: 'root'
@@ -7,10 +9,73 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 export class GameService {
 
   private gamesApiUrl = 'http://apis.manelme.com/replay/games';
+  private cardsApiUrl = 'http://apis.manelme.com/data/cards';
 
   constructor(private http: HttpClient) { }
 
   getGames(){
     return this.http.get(this.gamesApiUrl);
+  }
+
+  getCards(){
+    return this.http.get(this.cardsApiUrl);
+  }
+
+  getCardBackground(game):any{
+      let im_local,im_vs;
+      let is_local = false;
+      let is_vs = false;
+      let vargame = Object.entries(game.Plays);
+      for(let i = 0;i<vargame.length;i++){
+        for(let j = 0;j < vargame[i].length;j++){
+          //console.log(vargame[i][j]);
+          let jugada:any = vargame[i][j];
+          for(let k = 0;k< jugada.length;k++){
+            //console.log(jugada[k]);
+
+            if(jugada[k]["CardCode"]!==undefined && jugada[k]["CardCode"]!=="face"){
+
+              
+              if( jugada[k]["LocalPlayer"]=='true' && is_local==false){
+                im_local = jugada[k];
+                is_local = true;
+              }
+
+              if( jugada[k]["LocalPlayer"]=='false' && is_vs==false){
+                im_vs = jugada[k];
+                is_vs = true;
+              }
+
+              if(is_local && is_vs){
+                break;
+              }
+            }
+
+          }
+        }
+      }
+      return [im_local, im_vs];
+      let img_local,img_vs;
+    //console.log(im_local,im_vs);
+    /*
+    this.getCards().subscribe((datos:any) =>{
+        
+        for(let n = 0;n<datos.length;n++){
+          if(datos[n].cardCode == im_local.CardCode){
+            img_local = datos[n].assets[0].fullAbsolutePath;
+          }
+
+          if(datos[n].cardCode == im_vs.CardCode){
+            img_vs = datos[n].assets[0].fullAbsolutePath;
+          }
+
+          if(img_local !== undefined && img_vs !== undefined){
+            
+          
+          }
+        }
+    })
+    */
+
   }
 }
