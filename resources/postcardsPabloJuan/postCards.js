874b6d0d27ce3135ebcf98ca40f83865960d03a6
Manel <manel.mena@ual.es>
2020-05-17 10:39:06 +0200

    added bigglewater resources

--- resources/postcardsPabloJuan/postCards.js
+++ resources/postcardsPabloJuan/postCards.js
@@ -1,10 +1,12 @@
 var req = require('request');
 
-// Burrada por parte de pablo
-const cartas = require('fs')
-const regiones = require('fs')
+/* Burrada de Pablo */
+/* const cartas = require('fs')
+const regiones = require('fs') */
 
-regiones.readFile('../data/1.0.0/globals-en_us.json', (err, data) => {
+const fs = require('fs');
+
+fs.readFile('../data/1.1.0/globals-en_us.json', (err, data) => {
     if (err) throw err;
   
     let todo = JSON.parse(data);
@@ -18,17 +20,19 @@ regiones.readFile('../data/1.0.0/globals-en_us.json', (err, data) => {
     }); 
 });
 
-cartas.readFile('../data/1.0.0/set1-en_us.json', (err, data) => {
+fs.readFile('../data/1.1.0/set2-en_us.json', (err, data) => {
   if (err) throw err;
   
   let todocartas = JSON.parse(data);
-  todocartas.forEach(carta => {  
-    req.post({
-      url: 'http://apis.manelme.com/data/cards',
-      body: carta,
-      json: true
-    }, function(error, response, body){
-  });
+  todocartas.forEach(
+    (carta) => {  
+      req.post({
+        url: 'http://apis.manelme.com/data/cards',
+        body: carta,
+        json: true
+      }, function(error, response, body){
+    });
+  })
 
-  }); 
+
 });
